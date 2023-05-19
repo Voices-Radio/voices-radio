05760879a10a9889abe31b9a11918bacca0f44d5
Jeff Reiner <theundeadghost@gmail.com>
2023-05-19 15:18:16 +0200

    Make sure runtime is edge

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { getSettings } from "@/sanity.client";
 import { urlForImage } from "@/sanity.image";
 import { Metadata } from "next";
 
+export const runtime = "edge";
+
 export async function generateMetadata(): Promise<Metadata> {
   const { title, description, ogImage } = await getSettings();
 
