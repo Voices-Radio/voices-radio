e238a6fc97d9b87bf0a9f1f5cfa08351411993a9
Jeff Reiner <theundeadghost@gmail.com>
2023-05-19 15:31:49 +0200

    Remove media plugin for now

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,5 @@
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
-import { media } from "sanity-plugin-media";
 import { deskTool } from "sanity/desk";
 import { pageStructure, singletonPlugin } from "./plugins/settings";
 import { apiVersion, dataset, projectId } from "./sanity.env";
@@ -20,6 +19,5 @@ export default defineConfig({
     deskTool({ structure: pageStructure([settingsType]) }),
     singletonPlugin([settingsType.name]),
     visionTool({ defaultApiVersion: apiVersion }),
-    media(),
   ],
 });
