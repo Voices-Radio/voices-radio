817c1f38323f31381577f1e0ff5f021f40f901ae
Jeff Reiner <theundeadghost@gmail.com>
2023-05-19 14:58:15 +0200

    Add media plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
+import { media } from "sanity-plugin-media";
 import { deskTool } from "sanity/desk";
 import { pageStructure, singletonPlugin } from "./plugins/settings";
 import { apiVersion, dataset, projectId } from "./sanity.env";
@@ -18,5 +19,6 @@ export default defineConfig({
     deskTool({ structure: pageStructure([settingsType]) }),
     singletonPlugin([settingsType.name]),
     visionTool({ defaultApiVersion: apiVersion }),
+    media(),
   ],
 });
