dfc5078fac8f749045e97efadb00a74ac456d378
Jeff Reiner <theundeadghost@gmail.com>
2023-05-26 15:38:31 +0200

    Integrate kinfolk font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
+import localFont from "next/font/local";
 import "./globals.css";
+
+const kinfolk = localFont({
+  src: "./subset-BNKinfolkRounded.woff2",
+  display: "swap",
+  variable: "--font-kinfolk",
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${kinfolk.variable}`}>
       <body>{children}</body>
     </html>
   );
