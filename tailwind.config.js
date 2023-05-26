f4cca66f2943ec31050e14e0cbba0b2f9de26c3d
Jeff Reiner <theundeadghost@gmail.com>
2023-05-26 16:23:20 +0200

    Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,30 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-inter", ...defaultTheme.fontFamily.sans],
-        serif: ["var(--font-kinfolk)", ...defaultTheme.fontFamily.serif],
+        sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
+        kinfolk: ["var(--font-kinfolk)", ...defaultTheme.fontFamily.serif],
+      },
+      fontSize: {
+        "kinfolk-headline": [
+          "4.375rem",
+          {
+            lineHeight: "3.4375rem",
+          },
+        ],
+        "inter-text-semibold": [
+          "1.625rem",
+          {
+            lineHeight: "2.175rem",
+            fontWeight: "600",
+          },
+        ],
+        "inter-text-small": [
+          "1.125rem",
+          {
+            fontWeight: "600",
+            lineHeight: "1.375rem",
+          },
+        ],
       },
       colors: {
         voices: {
