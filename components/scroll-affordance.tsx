1a41b6a7f191161ad311567ff1bd4bc032ae79e7
Jeff Reiner <theundeadghost@gmail.com>
2023-05-27 12:47:40 +0200

    Working on the homepage

---
+++ components/scroll-affordance.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import ArrowDown from "@/icons/arrow-down";
+
+export default function ScrollAfforance({ target }: { target: `#${string}` }) {
+  function scrollIntoView() {
+    const element = document.querySelector(target);
+    if (element) element.scrollIntoView({ behavior: "smooth" });
+  }
+
+  return (
+    <button
+      onClick={scrollIntoView}
+      className="absolute left-1/2 -translate-x-1/2 bottom-4 p-2.5 h-11 w-11 rounded-full text-white bg-black"
+    >
+      <ArrowDown />
+    </button>
+  );
+}
