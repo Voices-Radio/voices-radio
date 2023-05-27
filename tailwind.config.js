03125eefdf9ea712219bc02e96bee817e120124b
Jeff Reiner <theundeadghost@gmail.com>
2023-05-27 16:38:38 +0200

    Some optimizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./icons/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
