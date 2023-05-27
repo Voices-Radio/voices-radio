8f00a5a64ea4b8227d4c1dde3b7b4f7f828c5b3f
Jeff Reiner <theundeadghost@gmail.com>
2023-05-27 13:55:55 +0200

    Stub in image carousel

--- app/(station)/community.tsx
+++ app/(station)/community.tsx
@@ -9,18 +9,20 @@ export default async function CommunitySection() {
       className="px-8 py-28 relative overflow-hidden bg-voices-beige"
       id="community"
     >
-      <div className="grid grid-cols-2">
-        <div className="flex flex-col items-center gap-10">
-          <h2 className="text-center font-kinfolk text-kinfolk-headline uppercase">
+      <div className="flex flex-col md:flex-row-reverse gap-10">
+        <div className="flex-1">IMAGE CAROUSEL HERE</div>
+
+        <div className="flex-1 flex flex-col items-center gap-10">
+          <h2 className="text-center font-kinfolk text-mobile-kinfolk-headline md:text-kinfolk-headline uppercase">
             {home.community_heading}
           </h2>
 
-          <p className="text-inter-text text-center max-w-lg">
+          <p className="text-mobile-inter-text md:text-inter-text text-center max-w-lg">
             {home.community_subheading}
           </p>
 
           <Link
-            className="rounded-full bg-transparent ring-4 ring-inset ring-black text-black px-20 py-[1.125rem] text-inter-text"
+            className="rounded-full bg-transparent ring-4 ring-inset ring-black text-black px-20 py-[1.125rem] text-mobile-inter-text md:text-inter-text"
             href={home.community_cta_url}
           >
             {home.community_cta_text}
