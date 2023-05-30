87dcbf8b43a4962d443155f6a03adf19f6bfc85a
Jeff Reiner <theundeadghost@gmail.com>
2023-05-30 19:44:18 +0200

    Change up the overlay to be wholey scrollable

--- components/schedule.tsx
+++ components/schedule.tsx
@@ -20,17 +20,17 @@ export default function Schedule({
       </Dialog.Trigger>
 
       <Dialog.Portal>
-        <Dialog.Overlay className="bg-black/20 backdrop-blur-xl fixed inset-0" />
-
-        <Dialog.Content className="fixed inset-0 focus:outline-none overflow-y-scroll">
-          <div className="max-w-5xl mx-auto p-10 mt-[4.5rem]">{children}</div>
+        <Dialog.Overlay className="bg-black/20 backdrop-blur-xl fixed inset-0 overflow-y-auto">
+          <Dialog.Content className="focus:outline-none">
+            <div className="max-w-5xl mx-auto p-10 mt-[4.5rem]">{children}</div>
+          </Dialog.Content>
 
           <Dialog.Close asChild>
-            <button className="fixed bottom-8 left-1/2 -translate-x-1/2 text-inter-mobile-text bg-black rounded-full px-20 py-4 text-white focus:outline-none">
+            <button className="sticky bottom-8 left-1/2 -translate-x-1/2 text-inter-mobile-text bg-black rounded-full px-20 py-4 text-white focus:outline-none">
               Close
             </button>
           </Dialog.Close>
-        </Dialog.Content>
+        </Dialog.Overlay>
       </Dialog.Portal>
     </Dialog.Root>
   );
