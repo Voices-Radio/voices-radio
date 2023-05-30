a972057f30f7ff7bc36065efc6bb4236c4041fb3
Jeff Reiner <theundeadghost@gmail.com>
2023-05-30 18:54:42 +0200

    Hookup chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Chat",
@@ -6,5 +7,32 @@ export const metadata: Metadata = {
 };
 
 export default async function ChatPage() {
-  return <div className="flex flex-col min-h-screen">INSERT CHAT HERE</div>;
+  return (
+    <Script
+      id="cid0020000285987468774"
+      strategy="lazyOnload"
+      src="https://st.chatango.com/js/gz/emb.js"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify({
+          handle: "voicesradiokx",
+          arch: "js",
+          styles: {
+            a: "000000",
+            b: 100,
+            c: "FFFFFF",
+            d: "FFFFFF",
+            k: "000000",
+            l: "000000",
+            m: "000000",
+            n: "FFFFFF",
+            p: "10",
+            q: "000000",
+            r: 100,
+            fwtickm: 1,
+            showx: 0,
+          },
+        }),
+      }}
+    />
+  );
 }
--- components/join-chat.tsx
+++ components/join-chat.tsx
@@ -1,19 +1,12 @@
 "use client";
 
 export default function JoinChat() {
-  function joinChat() {
-    window.open(
-      "/chat",
-      "voiceschatwindow",
-      "width=480,height=520,menubar=no,location=no,resizable=no,scrollbars=no,status=no"
-    );
-  }
-
   return (
     <button
-      onClick={joinChat}
+      onClick={() => {
+        window.open("/chat", "voiceschatwindow", "width=480,height=520");
+      }}
       className="px-6 bg-white rounded-full py-1.5 text-inter-text-small hidden relative md:block whitespace-nowrap ml-8"
-      type="button"
     >
       <span>Join Chat</span>
 
