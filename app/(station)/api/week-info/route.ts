1d044a268ff589a0f2517b20e9611f8d6da572a9
Jeff Reiner <theundeadghost@gmail.com>
2023-06-15 18:07:16 +0200

    Get caching back

--- app/(station)/api/week-info/route.ts
+++ app/(station)/api/week-info/route.ts
@@ -14,7 +14,7 @@ import { NextResponse } from "next/server";
 
 export const runtime = "edge";
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
