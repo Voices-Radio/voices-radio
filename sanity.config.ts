7e080ad0639964b961c086eba3636a852dd2ee4d
Jeff Reiner <theundeadghost@gmail.com>
2023-08-16 12:28:29 +0200

    Tweak

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,14 +8,20 @@ import partnerType from "./schemas/partner";
 import settingsType from "./schemas/settings";
 import pageSectionType from "./schemas/page-section";
 
+const schemaTypes = [
+  settingsType,
+  partnerType,
+  homeType,
+  aboutType,
+  pageSectionType,
+];
+
 export default defineConfig({
   basePath: "/studio",
   projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   title: "Studio",
   dataset: env.NEXT_PUBLIC_SANITY_DATASET,
-  schema: {
-    types: [settingsType, partnerType, homeType, aboutType, pageSectionType],
-  },
+  schema: { types: schemaTypes },
   plugins: [
     deskTool({
       structure: pageStructure([settingsType, homeType, aboutType]),
