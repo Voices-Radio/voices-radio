7f915a056bf332a3b1fde932cc9dfecfd18985c3
Jeff Reiner <theundeadghost@gmail.com>
2023-08-24 17:06:44 +0200

    Working on the about page

--- app/(station)/about/page.tsx
+++ app/(station)/about/page.tsx
@@ -55,8 +55,10 @@ export default async function AboutPage() {
         </div>
       </section>
 
-      <section className="relative overflow-hidden bg-voices-purple text-white px-6 pb-12 pt-6 md:px-8 md:py-28">
-        <div className="max-w-5xl mx-auto space-y-8 md:space-y-10">
+      <section className="z-auto relative overflow-hidden bg-voices-purple text-white px-8 py-40 pb-28">
+        <div className="absolute inset-0 backdrop-blur-xl" />
+
+        <div className="relative max-w-5xl mx-auto space-y-8 md:space-y-10">
           <h2 className="text-mobile-inter-text text-center">
             {about.bookings_heading}
           </h2>
@@ -71,6 +73,18 @@ export default async function AboutPage() {
               </span>
             ))}
           </div>
+        </div>
+
+        <div className="pointer-events-none absolute -top-0.5 left-0 right-0 max-w-none">
+          <svg
+            className="rotate-180 text-voices-beige xl:h-[9.375rem] xl:w-full"
+            viewBox="0 0 1440 150"
+            preserveAspectRatio="none"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <use href="#wave-bottom" fill="currentColor" />
+          </svg>
         </div>
       </section>
 
@@ -89,16 +103,28 @@ export default async function AboutPage() {
       <section className="relative aspect-[4/3] md:aspect-[16/8]">
         <Image
           alt=""
-          blurDataURL={about.hero_image.lqip}
-          className="object-cover object-bottom"
+          blurDataURL={about.community_image.lqip}
+          className="object-cover object-center"
           draggable={false}
           fill
           placeholder="blur"
           priority
           quality={85}
           sizes="100vw"
-          src={urlForImage(about.hero_image).url()}
+          src={urlForImage(about.community_image).url()}
         />
+
+        <div className="pointer-events-none absolute -top-0.5 left-0 right-0 max-w-none">
+          <svg
+            className="rotate-180 text-voices-beige xl:h-[9.375rem] xl:w-full"
+            viewBox="0 0 1440 150"
+            preserveAspectRatio="none"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <use href="#wave-bottom" fill="currentColor" />
+          </svg>
+        </div>
       </section>
 
       <section className="relative overflow-hidden bg-voices-beige px-6 pb-12 pt-6 md:px-8 md:py-28">
