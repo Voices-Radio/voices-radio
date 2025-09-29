92ab0a3904c1ffbe02bd00f869f0a08588a4ded6
jonslow123 <onslow.jack@yahoo.com>
2025-09-29 21:46:11 +0100

    Logo

--- app/podcast/page.tsx
+++ app/podcast/page.tsx
@@ -161,7 +161,7 @@ const PodcastPage = () => {
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center space-x-2">
               <Image 
-                src="/voices.svg" 
+                src="/VOICESLOGO_LIGHTBOX.svg" 
                 alt="Voices Studio Logo" 
                 width={32}
                 height={32}
